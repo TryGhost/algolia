5f2db92abac311f57a2b2d3b367130f2d4b6de4b
Aileen Nowak <AileenCGN@gmail.com>
2020-04-23 22:05:58 +1200

    🎉 Port over Netlify Functions code for post.published

---
+++ packages/algolia-netlify/functions/post-published.js
@@ -0,0 +1,28 @@
+const indexFactory = require('@tryghost/algolia-indexer');
+const parserFactory = require('@tryghost/algolia-fragmenter');
+
+exports.handler = (event, context, callback) => {
+    const algoliaSettings = {
+        active: process.env.ALGOLIA_ACTIVE === 'TRUE',
+        applicationID: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_API_KEY,
+        index: process.env.ALGOLIA_INDEX
+    };
+
+    const post = JSON.parse(event.body).post.current;
+    const index = indexFactory(algoliaSettings);
+
+    if (index.connect() && parserFactory().parse(post, index)) {
+        index
+            .save()
+            .then(() => {
+                callback(null, {
+                    statusCode: 200,
+                    body: `GhostAlgolia: post "${post.title}" has been added to the index.`
+                });
+            })
+            .catch((err) => {
+                callback(err);
+            });
+    }
+};
