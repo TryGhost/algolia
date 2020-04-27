926034f652afebfe155d9a6d43cf6a6e5362fac2
Aileen Nowak <AileenCGN@gmail.com>
2020-04-27 20:44:36 +1200

    Disable code to publish versions

--- packages/algolia-netlify/functions/post-published.js
+++ packages/algolia-netlify/functions/post-published.js
@@ -1,28 +1,28 @@
-import indexFactory from '@tryghost/algolia-indexer';
-import parserFactory from '@tryghost/algolia-fragmenter';
+// import indexFactory from '@tryghost/algolia-indexer';
+// import parserFactory from '@tryghost/algolia-fragmenter';
 
-exports.handler = (event, context, callback) => {
-    const algoliaSettings = {
-        active: process.env.ALGOLIA_ACTIVE === 'TRUE',
-        applicationID: process.env.ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_API_KEY,
-        index: process.env.ALGOLIA_INDEX
-    };
+exports.handler = () => {
+    // const algoliaSettings = {
+    //     active: process.env.ALGOLIA_ACTIVE === 'TRUE',
+    //     applicationID: process.env.ALGOLIA_APP_ID,
+    //     apiKey: process.env.ALGOLIA_API_KEY,
+    //     index: process.env.ALGOLIA_INDEX
+    // };
 
-    const post = JSON.parse(event.body).post.current;
-    const index = indexFactory(algoliaSettings);
+    // const post = JSON.parse(event.body).post.current;
+    // const index = indexFactory(algoliaSettings);
 
-    if (index.connect() && parserFactory().parse(post, index)) {
-        index
-            .save()
-            .then(() => {
-                callback(null, {
-                    statusCode: 200,
-                    body: `GhostAlgolia: post "${post.title}" has been added to the index.`
-                });
-            })
-            .catch((err) => {
-                callback(err);
-            });
-    }
+    // if (index.connect() && parserFactory().parse(post, index)) {
+    //     index
+    //         .save()
+    //         .then(() => {
+    //             callback(null, {
+    //                 statusCode: 200,
+    //                 body: `GhostAlgolia: post "${post.title}" has been added to the index.`
+    //             });
+    //         })
+    //         .catch((err) => {
+    //             callback(err);
+    //         });
+    // }
 };
