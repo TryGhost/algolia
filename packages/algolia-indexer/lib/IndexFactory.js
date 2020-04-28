89289852ccc386883e6766414a96e6a137bc1afd
Aileen Nowak <AileenCGN@gmail.com>
2020-04-28 16:29:59 +1200

    🚚 Better node.js structure

---
+++ packages/algolia-indexer/lib/IndexFactory.js
@@ -0,0 +1,52 @@
+const algoliaSearch = require('algoliasearch');
+
+// Any defined settings will override those in the algolia UI
+const REQUIRED_SETTINGS = {
+    // We chunk our pages into small algolia entries, and mark them as distinct by slug
+    // This ensures we get one result per page, whichever is ranked highest
+    distinct: true,
+    attributeForDistinct: `slug`,
+    // This ensures that chunks higher up on a page rank higher
+    customRanking: [`desc(customRanking.heading)`, `asc(customRanking.position)`],
+    // Defines the order algolia ranks various attributes in
+    searchableAttributes: [`title`, `headings`, `html`, `url`, `tags.name`, `tags`]
+};
+
+class IndexFactory {
+    constructor(algoliaSettings = {}) {
+        if (!algoliaSettings.apiKey || !algoliaSettings.appId || !algoliaSettings.index || algoliaSettings.index.length < 1) {
+            throw new Error('Algolia appId, apiKey, and index is required!');
+        }
+        this.index = [];
+        this.options = algoliaSettings;
+
+        this.options.indexSettings = algoliaSettings.indexSettings || REQUIRED_SETTINGS;
+    }
+    initClient() {
+        this.client = algoliaSearch(this.options.appId, this.options.apiKey);
+    }
+    initIndex() {
+        this.initClient();
+        this.index = this.client.initIndex(this.options.index);
+    }
+    async setSettingsForIndex() {
+        this.initIndex();
+        return await this.index.setSettings(this.options.indexSettings)
+            .then(() => this.index.getSettings());
+    }
+    async save(fragments) {
+        console.log('save -> fragments', fragments);
+        return await this.index.saveObjects(fragments)
+            .then(res => console.log(`saved to Algolia: `, res)); // eslint-disable-line no-console
+    }
+    delete(post) {
+        return this.index.deleteBy(post.attributes.uuid, {
+            restrictSearchableAttributes: 'post_uuid'
+        });
+    }
+    countRecords() {
+        return this.index.search({query: '', hitsPerPage: 0}).then(queryResult => queryResult.nbHits);
+    }
+}
+
+module.exports = IndexFactory;
--- packages/algolia-netlify/functions/post-published.js
+++ packages/algolia-netlify/functions/post-published.js
@@ -13,15 +13,38 @@ exports.handler = (event, context, callback) => {
     };
 
     const post = JSON.parse(event.body).post.current;
+
+    // TODO: move to a util
+    const algoliaPost = {
+        objectID: post.uuid,
+        slug: post.slug,
+        html: post.html,
+        image: post.feature_image,
+        title: post.title,
+        tags: []
+    };
+
+    post.tags.forEach((tag) => {
+        algoliaPost.tags.push({name: tag.name, slug: tag.slug});
+    });
+
     const node = [];
-    node.push(post);
+
+    node.push(algoliaPost);
+
+    // Instanciate the Algolia indexer, which connects to Algolia and
+    // sets up the settings for the index.
     const index = new IndexFactory(algoliaSettings);
+
+    // Fragmenter needs an Array to reduce
     const fragments = node.reduce(transforms.fragmentTransformer, []);
 
     index
         .setSettingsForIndex()
-        .then(() => {
-            index
+        .then((settings) => {
+            console.log('Index set up with following settings: ', settings); // eslint-disable-line no-console
+
+            return index
                 .save(fragments).then(() => {
                     callback(null, {
                         statusCode: 200,
