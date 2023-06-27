f039b40ae1a8e2744f48ee4d4a55df0a60d2cc73
Aileen Booker <AileenCGN@gmail.com>
2023-06-27 12:44:35 -0400

    Made key optional to prevent breaking change

--- packages/algolia-netlify/functions/post-published.js
+++ packages/algolia-netlify/functions/post-published.js
@@ -4,7 +4,8 @@ const transforms = require('@tryghost/algolia-fragmenter');
 exports.handler = async (event) => {
     const {key} = event.queryStringParameters;
 
-    if (key !== process.env.NETLIFY_KEY) {
+    // TODO: Deprecate this in the future and make the key mandatory
+    if (key && key !== process.env.NETLIFY_KEY) {
         return {
             statusCode: 401,
             body: `Unauthorized`
